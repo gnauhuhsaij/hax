83fcf13f993b38f4120d30db3239d1eece24cbce
Jiashu <jiashuhuang@ucsb.edu>
2025-02-23 00:18:06 -0800

    Add gitignore file

--- firstapp/frontend/src/components/Sidebar.js
+++ firstapp/frontend/src/components/Sidebar.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Sidebar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Sidebar.css";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -10,7 +10,7 @@ const Sidebar = () => {
   };
 
   return (
-    <div className={`sidebar ${isOpen ? 'open' : 'collapsed'}`}>
+    <div className={`sidebar ${isOpen ? "open" : "collapsed"}`}>
       <button className="toggle-button" onClick={toggleSidebar}>
         <img src="/icons/collapse.svg" alt="Collapse Icon" className="icon" />
       </button>
@@ -20,32 +20,43 @@ const Sidebar = () => {
           <ul>
             <li>
               <Link to="/">
-                <img src="/icons/home.svg" alt="Home Icon" className="icon" /> Home
+                <img src="/icons/home.svg" alt="Home Icon" className="icon" />{" "}
+                Home
               </Link>
             </li>
             <li>
               <Link to="/community">
-                <img src="/icons/global.svg" alt="Community Icon" className="icon" /> Community
+                <img
+                  src="/icons/global.svg"
+                  alt="Community Icon"
+                  className="icon"
+                />{" "}
+                Community
               </Link>
             </li>
             <li>
               <Link to="/saved">
-                <img src="/icons/save.svg" alt="Saved Icon" className="icon" /> Saved
+                <img src="/icons/save.svg" alt="Saved Icon" className="icon" />{" "}
+                Saved
               </Link>
             </li>
             <li>
               <Link to="/reward">
-                <img src="/icons/coin.svg" alt="Reward Icon" className="icon" /> Reward
+                <img src="/icons/coin.svg" alt="Reward Icon" className="icon" />{" "}
+                Reward
               </Link>
             </li>
           </ul>
           <div className="profile-section">
-            <img src="/icons/ellipse.svg" alt="Account Icon" className="account-icon" />
+            <img
+              src="/icons/ellipse.svg"
+              alt="Account Icon"
+              className="account-icon"
+            />
             <div className="descriptions">
               <h3 className="username">William Pan</h3>
               <p className="days">20 Days at Do.ai</p>
             </div>
-            
           </div>
         </>
       )}
